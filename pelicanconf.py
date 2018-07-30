963ff27c357916410af950c1c423b624e2c12292
tofof <github@tofof.com>
2018-07-30 17:07:44 -0500

    social icons, unicode strings

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,7 +7,6 @@ PLUGINS=['summary', 'sitemap', "tag_cloud"]
 
 AUTHOR = u'Brian James'
 SITENAME = u'Negative One'
-# SITEURL = "http://localhost:8000"
 SITEURL = "http://tofof.com"
 
 PATH = 'content'
@@ -20,7 +19,7 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-TIMEZONE = 'America/Chicago'
+TIMEZONE = u'America/Chicago'
 DEFAULT_LANG = u'en'
 DEFAULT_PAGINATION = 5
 TYPOGRIFY = True
@@ -32,27 +31,25 @@ THEME = "themes/euler-theme"
 ### 
 ### euler-theme Config Begins
 ###
-##GOOGLE_ANALYTICS_CODE: your Google Analytics stats
+ARCHIVES_URL = 'archives/'
 SITELOGO = '/images/eipi_trans_white.png'
-SITETAGLINE = "by Tofof"
-SITEDESCR = "Tirades, trials, tasks and trivialities of a 21<sup>st</sup>-century polymath"
+SITETAGLINE = u"by Tofof"
+SITEDESCR = u"Tirades, trials, tasks and trivialities of a 21<sup>st</sup>-century polymath"
 
 DISQUS_SITENAME = "tofof"
 
-EMAIL_URL = 'web@tofof.com'
-
-ARCHIVES_URL = 'archives/'
-
-LICENSE_NAME = '<img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/80x15.png" /></a>'
+LICENSE_IMAGE = '<img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/80x15.png" />'
+LICENSE_NAME = u'CC BY-SA 4.0'
 LICENSE_URL  = 'http://creativecommons.org/licenses/by-sa/4.0/'
 
-#TWITTER_URL = 'http://twitter.com/tofof'
+EMAIL_URL = 'web@tofof.com'
+TWITTER_URL = 'https://twitter.com/tofof'
 #GOOGLE_URL = 'https://plus.google.com/105275131994304318274/'
-#GITHUB_URL: URL to your GitHub page, for the social icons
-#LINKEDIN_URL: URL to your LinkedIn page, for the social icons
-#FACEBOOK_URL: URL to your Facebook page, for the social icons
-#FLICKR_URL: URL to your Flickrpage, for the social icons
-#CONTACT_URL: the link to your contact page
+GITHUB_URL = 'https://github.com/tofof'
+#LINKEDIN_URL = URL to your LinkedIn page, for the social icons
+#FACEBOOK_URL = URL to your Facebook page, for the social icons
+#CONTACT_URL = the link to your contact page
+##GOOGLE_ANALYTICS_CODE: your Google Analytics stats
 ### 
 ### euler-theme Config Ends
 ###
@@ -62,16 +59,6 @@ LICENSE_URL  = 'http://creativecommons.org/licenses/by-sa/4.0/'
 LINKS = (
 	('wdtz / Will Dietz', 'http://wdtz.org'),
 	('nohelix / Noelle James', 'http://nohelix.com'),
-)
-
-# Social widget
-SOCIAL = (
-#	('Email', 'mailto:wdietz2@uiuc.edu', 'fa-envelope'),
-#	('Twitter', 'http://twitter.com/wdtz', 'fa-twitter'),
-#	('Github', 'http://github.com/dtzWill', 'fa-github'),
-#	('Facebook', 'http://facebook.com/dtzWill', 'fa-facebook-square'),
-#	('Google-Plus', 'http://wdtz.org/+', 'fa-google-plus'),
-#	('##uiuc on Freenode', 'http://webchat.freenode.net/?channels=%23%23uiuc', 'fa-comment'),
 )
 
 # Uncomment following line if you want document-relative URLs when developing
